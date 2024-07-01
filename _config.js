11d066e4602a721aaa3700f0f663508ca32ff4af
Gerrykiptoo <gerrykiptoo@gmail.com>
2024-07-01 09:35:55 +0300

    confured the config.js file successfully

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://faithkhaoya95:Gerry,,,254@tmcluster.n8lkgte.mongodb.net/',
-    development: 'mongodb+srv://faithkhaoya95:Gerry,,,254@tmcluster.n8lkgte.mongodb.net/',
-    test: 'mongodb+srv://faithkhaoya95:Gerry,,,254@tmcluster.n8lkgte.mongodb.net/',
+    production: 'mongodb+srv://gerrybiwott:Gerry,,,254@gkcluster.onzrc3b.mongodb.net/',
+    development: 'mongodb+srv://gerrybiwott:Gerry,,,254@gkcluster.onzrc3b.mongodb.net/',
+    test: 'mongodb+srv://gerrybiwott:Gerry,,,254@gkcluster.onzrc3b.mongodb.net/',
 }
 module.exports = config;
